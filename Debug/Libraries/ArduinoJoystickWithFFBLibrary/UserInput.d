2a46aa4f1de89a6d3e2b6e3f989f415b756c7fec
Log1cFX <raffisand@gmail.com>
2026-01-19 21:45:13 +0100

    gitignore

--- Debug/Libraries/ArduinoJoystickWithFFBLibrary/UserInput.d
+++
@@ -1,6 +0,0 @@
-Libraries/ArduinoJoystickWithFFBLibrary/UserInput.o: \
- ../Libraries/ArduinoJoystickWithFFBLibrary/UserInput.cpp \
- ../Libraries/ArduinoJoystickWithFFBLibrary/UserInput.h \
- ../Libraries/ArduinoJoystickWithFFBLibrary/HIDReportType.h
-../Libraries/ArduinoJoystickWithFFBLibrary/UserInput.h:
-../Libraries/ArduinoJoystickWithFFBLibrary/HIDReportType.h:
