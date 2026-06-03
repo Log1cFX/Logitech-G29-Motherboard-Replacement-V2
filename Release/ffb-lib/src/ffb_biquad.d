fea4e056a784fa30feff550df6c34635d2ac8d64
Log1cFX <raffisand@gmail.com>
2026-06-04 01:50:31 +0200

    .gitignore remove release

--- Release/ffb-lib/src/ffb_biquad.d
+++
@@ -1,3 +0,0 @@
-ffb-lib/src/ffb_biquad.o: ../ffb-lib/src/ffb_biquad.cpp \
- C:/Users/raffi/STM32CubeIDE/workspace_1.16.0/g29-MB-Replacement/ffb-lib/include/ffb/ffb_biquad.h
-C:/Users/raffi/STM32CubeIDE/workspace_1.16.0/g29-MB-Replacement/ffb-lib/include/ffb/ffb_biquad.h:
